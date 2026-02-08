4dfb663c7f4ea6d12b1eb6dc05fbbd4a173136f5
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2026-02-07 23:28:46 -0800

    Re-enable Logbook and use Apache HC5
    Spring Cloud OpenFeign dropped support of OkHttp

--- tea-service/src/main/java/org/example/teahouse/tea/config/LogBookConfig.java
+++ tea-service/src/main/java/org/example/teahouse/tea/config/LogBookConfig.java
@@ -1,10 +1,18 @@
 package org.example.teahouse.tea.config;
 
-//@Configuration
+import org.springframework.cloud.openfeign.clientconfig.HttpClient5FeignConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.httpclient5.LogbookHttpRequestInterceptor;
+import org.zalando.logbook.httpclient5.LogbookHttpResponseInterceptor;
+
+@Configuration
 public class LogBookConfig {
-//    @Bean
-//    public OkHttpClient.Builder okHttpClientBuilder(Logbook logbook) {
-//        return new OkHttpClient.Builder()
-//            .addNetworkInterceptor(new LogbookInterceptor(logbook));
-//    }
+    @Bean
+    public HttpClient5FeignConfiguration.HttpClientBuilderCustomizer httpClientBuilderCustomizer(Logbook logbook) {
+        return builder -> builder
+            .addRequestInterceptorFirst(new LogbookHttpRequestInterceptor(logbook))
+            .addResponseInterceptorFirst(new LogbookHttpResponseInterceptor());
+    }
 }
