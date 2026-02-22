9ef013e249af23cd2818d95c3918038a1c92ce02
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2026-02-21 17:22:13 -0800

    Remove JacksonJsonHttpMessageConverter workaround
    I cannot make the app not broken, if I create and configure
    JacksonJsonHttpMessageConverter, a subset of these will be broken:
    - SSE
    - Springdoc/Swagger
    - SBOM endpoint
    - Spring Boot Admin
    
    Without it, the broken things (so far):
    - Spring Framework throws a HttpMediaTypeNotAcceptableException
    - The @ExceptionHandler is called twice
    
    See https://github.com/spring-projects/spring-boot/issues/49265

--- core/src/main/java/org/example/teahouse/core/error/ExceptionHandlerAutoConfiguration.java
+++ core/src/main/java/org/example/teahouse/core/error/ExceptionHandlerAutoConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
-import tools.jackson.databind.json.JsonMapper;
 
 @AutoConfiguration
 public class ExceptionHandlerAutoConfiguration {
@@ -15,11 +13,5 @@ public class ExceptionHandlerAutoConfiguration {
     @ConditionalOnBean(Tracer.class)
     CommonExceptionHandler commonExceptionHandler(Tracer tracer) {
         return new CommonExceptionHandler(tracer);
-    }
-
-    // Workaround for https://github.com/spring-projects/spring-boot/issues/49265
-    @Bean
-    JacksonJsonHttpMessageConverter jacksonJsonHttpMessageConverter(JsonMapper.Builder builder) {
-        return new JacksonJsonHttpMessageConverter(builder);
     }
 }
